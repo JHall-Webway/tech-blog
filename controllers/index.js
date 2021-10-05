8a90c9581f1ea913e3e575e00b17add24fe38723
JHallUofA <83625694+JHallUofA@users.noreply.github.com>
2021-10-04 22:57:42 -0700

    Add main handlebars template

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,8 @@
 const router = require('express').Router();
 
 router.use('/api', require('./api'));
+router.get('/', (req, res) => {
+    res.render('login')
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+const helpers = require('./utils/helpers');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({ helpers });
 const sequelize = require('./config/connection');
 const routes = require('./controllers');
 
+app.engine('handlebars', hbs.engine);
+
+app.set('view engine', 'handlebars');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
